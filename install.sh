35301244787dcff7f5c1b9372024dfa536fbdb1c
Renxing <flyrenxing@qq.com>
2024-07-31 22:35:57 +0800

    fix

--- install.sh
+++ install.sh
@@ -137,10 +137,9 @@ sed -i "s/ENABLE_OPENDKIM=1/ENABLE_OPENDKIM=0/g" mailserver.env
 docker compose up -d
 
 # 添加邮箱账户和配置DKIM
-sleep 5
 ./setup.sh email add $Postmaster@${DomainName} 6c9W9LM65eGjM7tmHv
-sleep 5
 ./setup.sh config dkim keysize 1024 domain $DomainName
+sleep 5
 
 # 读取 DKIM DNS 记录
 docker cp mailserver:/tmp/docker-mailserver/rspamd/dkim/rsa-1024-mail-${DomainName}.public.dns.txt ./
