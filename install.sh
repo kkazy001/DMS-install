3191d88f138678423574d95e1f117d55e4f3987a
Renxing <flyrenxing@qq.com>
2024-08-11 15:44:39 +0800

    ok

--- install.sh
+++ install.sh
@@ -115,7 +115,7 @@ docker run --rm \
   certbot/certbot certonly --standalone -d mail.${domains[0]} --agree-tos --no-eff-email --register-unsafely-without-email -v
 
 # 安装DMS
-DMS_GITHUB_URL="https://raw.githubusercontent.com/kkazy001/DMS-install/main"
+DMS_GITHUB_URL="https://raw.githubusercontent.com/flyrenxing/DMS-install/main"
 wget "${DMS_GITHUB_URL}/compose.yaml"
 wget "${DMS_GITHUB_URL}/mailserver.env"
 wget "${DMS_GITHUB_URL}/setup.sh"
